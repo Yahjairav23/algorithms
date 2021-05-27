a97a1c136829b111b7b9fce20606b593e9c989b8
Yahjairav23 <vyahjaira23@gmail.com>
2021-05-26 21:47:46 -0400

    build towers sudo code

---
+++ Build Tower/BuildTower.js
@@ -0,0 +1,43 @@
+// Build Tower
+// Build Tower by the following given argument:
+// number of floors (integer and always greater than 0).
+
+// Tower block is represented as *
+
+// Python: return a list;
+// JavaScript: returns an Array;
+// C#: returns a string[];
+// PHP: returns an array;
+// C++: returns a vector<string>;
+// Haskell: returns a [String];
+// Ruby: returns an Array;
+// Lua: returns a Table;
+// Have fun!
+
+// for example, a tower of 3 floors looks like below
+
+// [
+//   '  *  ', 
+//   ' *** ', 
+//   '*****'
+// ]
+// and a tower of 6 floors looks like below
+
+// [
+//   '     *     ', 
+//   '    ***    ', 
+//   '   *****   ', 
+//   '  *******  ', 
+//   ' ********* ', 
+//   '***********'
+// ]
+
+
+function towerBuilder(nFloors) {
+    // build here
+  //   * on each level == index + (index+1)
+  //   create array with nFloors + (nFloors-1) amount of * at each index
+  //   loop nFloors amount of times
+  //   starting from end of array, spaces if 0 on each side and goes up +1 each step closer to beginning of array
+    
+  }
